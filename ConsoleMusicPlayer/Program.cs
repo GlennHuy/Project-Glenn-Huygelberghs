d07486878b64b745e68ef9a1f3f658c83e0f56fb
Glenn H <huygelberghs.g@hotmail.be>
2022-06-08 15:46:41 +0200

    Add project files.

---
+++ ConsoleMusicPlayer/Program.cs
@@ -0,0 +1,6 @@
+﻿using WMPLib;
+
+WindowsMediaPlayer player = new WindowsMediaPlayer();
+string musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+player.URL = System.IO.Path.Combine(musicFolder, "Cathedrals of Mourning.mp3");
+player.controls.play();
